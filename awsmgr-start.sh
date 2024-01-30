051609d696641a0dbdf1b94bed814060f3525152
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-01-30 10:05:18 -0700

    issue with startup script

--- awsmgr-start.sh
+++ awsmgr-start.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+AWSMGR_DIR=$(npm list -g awsmgr --parseable)
 
-npm run start-server
+if [ "x${AWSMGR_DIR}" == "x" ]; then
+  echo "ERROR: npm package \"awsmgr may be missing..\""
+  echo " $ npm list to check if installed."
+  exit 1
+elif [ ! -d "${AWSMGR_DIR}" ]; then
+  echo "ERROR: the directory ${AWSMGR_DIR} does not exist"
+  exit 1
+fi
+
+cd ${AWSMGR_DIR} && npm run start-server

9e624428931677ed34c91f0564aa49436aad7303
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-01-31 22:38:18 -0700

    fix for server.js and some tweaks to our package

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ require("dotenv").config({ path: "./.env.local" });
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
-const socketPath = process.env.NODE_SOCK;
+const socketPath = "/tmp/express.sock";
+if (process.env.NODE_SOCK) {
+  socketPath = process.env.NODE_SOCK;
+}
 
 app.prepare().then(() => {
   const server = express();

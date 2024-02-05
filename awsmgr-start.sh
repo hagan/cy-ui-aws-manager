7741b14905e396ddf8308768fc1a4601e92ab350
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-04 21:58:31 -0700

    befuddled with the ux/express servers stuff.

--- awsmgr-start.sh
+++ awsmgr-start.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-AWSMGR_DIR=$(npm list -g awsmgr --parseable)
+
+if [ $(whoami) != 'node' ]; then
+  echo "ERROR: must run awsmgr-start as 'node' user. Currently running as: $(whoami)"
+  exit 1
+fi
+
+cd $HOME
+export PATH=$HOME/node_modules/.bin:$PATH
+AWSMGR_DIR=$(npm list awsmgr --parseable)
 
 if [ "x${AWSMGR_DIR}" == "x" ]; then
   echo "ERROR: npm package \"awsmgr may be missing..\""
@@ -8,8 +16,7 @@ if [ "x${AWSMGR_DIR}" == "x" ]; then
 elif [ ! -d "${AWSMGR_DIR}" ]; then
   echo "ERROR: the directory ${AWSMGR_DIR} does not exist"
   exit 1
+else
+  echo "cd ${AWSMGR_DIR} and start ExpressJS server..."
+  cd ${AWSMGR_DIR} && /usr/bin/npm run start-server
 fi
-#su -l node -s /bin/bash -c "PATH=/usr/local/bin:/usr/local/bin:/usr/bin:/bin && which node"
-# su - node -s /bin/bash -c "cd ${AWSMGR_DIR} && /usr/local/bin/npm install"
-# su - node -s /bin/bash -c "cd ${AWSMGR_DIR} && /usr/local/bin/npm run start-server"
-# cd ${AWSMGR_DIR} && npm run start-server
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const next = require("next");
 const fs = require("fs");
+const path = require("path");
+// const { exec } = require('child_process');
 
-// Load environment vars
-require("dotenv").config({ path: "./.env.local" });
+// $HOME/.env.local <= where we can put our environment vars
+const localEnvPath = path.join(process.env.HOME, ".env.local");
+var socketPath = "/tmp/express.sock";
+
+fs.access(localEnvPath, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.error("$HOME/.env.local not found.");
+  } else {
+    require("dotenv").config({ path: localEnvPath });
+    if (process.env.NODE_SOCK) {
+      socketPath = process.env.NODE_SOCK;
+    }
+    // Your server setup code goes here
+  }
+});
 
 // Maybe need to add tests to check for Environment variables required for project?
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-let socketPath = "/tmp/express.sock";
+var socketPath = "/tmp/express.sock";
 if (process.env.NODE_SOCK) {
   socketPath = process.env.NODE_SOCK;
 }
@@ -29,11 +44,25 @@ app.prepare().then(() => {
   });
 
   server.listen(socketPath, (err) => {
+    if (err) throw err;
+    // srv_cmd = "getent group www-data | cut -d: -f3"
+    // exec(srv_cmd, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error while running ${srv_cmd}: ${error}`);
+    //     return;
+    //   }
+    //   const wwwDataGID = parseInt(stdout.trim(), 10);
+    //   fs.chown(socketPath, process.getuid(), wwwDataGID, (err) => {
+    //     if (err) {
+    //       console.error(`chown error: ${err}`);
+    //       return;
+    //     }
+    //   });
     fs.chmod(socketPath, 0o664, (err) => {
       if (err) throw err;
       console.log(`Set permissions for socket ${socketPath} to 0664`);
     });
-    if (err) throw err;
+    // });
     console.log(` Ready on ${socketPath}`);
   });
 });

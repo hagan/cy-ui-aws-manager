078dcf0e15bd9127433669ed286492b951c434c9
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-28 09:00:03 -0700

    fix for terminal page on remote system

--- src/app/(CSR)/terminal/TerminalPage.tsx
+++ src/app/(CSR)/terminal/TerminalPage.tsx
@@ -1,35 +1,52 @@
 // app/(SSR)/terminal/page.tsx
 "use client";
-import dynamic from 'next/dynamic';
-import { useEffect } from 'react';
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 // import TerminalComponent from "@components/Terminal";
-const TerminalComponent = dynamic(() => import('@components/Terminal'), { ssr: false });
+const TerminalComponent = dynamic(() => import("@components/Terminal"), {
+  ssr: false,
+});
 
 export default async function TerminalPage() {
+  const [shellUrl, setShellUrl] = useState("");
 
-    useEffect(() => {
-        const disableCtrlShiftC = (event: KeyboardEvent) => {
-            if (event.ctrlKey && event.shiftKey && (event.key === 'C' || event.key === 'c')) {
-                event.preventDefault();
-                // You can also implement your custom logic here if needed
-                console.log('Ctrl+Shift+C is disabled on this page.');
-            }
-        };
+  useEffect(() => {
+    const protocol = window.location.protocol;
+    const hostname = window.location.hostname;
 
-        document.addEventListener('keydown', disableCtrlShiftC);
+    const port = window.location.port ? `:${window.location.port}` : "";
+    const path = "/shell";
 
-        // Clean up the event listener when the component unmounts
-        return () => {
-            document.removeEventListener('keydown', disableCtrlShiftC);
-        };
-    }, []);
+    const fullShellPath = `${protocol}//${hostname}${port}${path}`;
 
-    return (
-        <div style={{ padding: "20px", height: "90vh", width: "90vw" }}>
-            {/* @TODO: FIGURE OUT HOW TO GET THE REAL PATH FOR VICE/DE */}
-            <h1>Terminal</h1>
-            <TerminalComponent src="http://localhost/shell" />
-        </div>
-    );
+    setShellUrl(fullShellPath);
+
+    const disableCtrlShiftC = (event: KeyboardEvent) => {
+      if (
+        event.ctrlKey &&
+        event.shiftKey &&
+        (event.key === "C" || event.key === "c")
+      ) {
+        event.preventDefault();
+        // You can also implement your custom logic here if needed
+        console.log("Ctrl+Shift+C is disabled on this page.");
+      }
+    };
+
+    document.addEventListener("keydown", disableCtrlShiftC);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("keydown", disableCtrlShiftC);
+    };
+  }, []);
+
+  return (
+    <div style={{ padding: "20px", height: "90vh", width: "90vw" }}>
+      {/* @TODO: FIGURE OUT HOW TO GET THE REAL PATH FOR VICE/DE */}
+      <h1>Terminal</h1>
+      <TerminalComponent src={shellUrl} />
+    </div>
+  );
 }
